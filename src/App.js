70c897abb8c3bca67dd68d151d12f18604a6d8f8
Iga Chmielarczyk <iga.chmielarczyk@gmail.com>
2021-03-14 10:15:49 +0100

    comleted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import './App.css';
 import Chart from './Chart';
-import {useForm} from "react-hook-form";
 
 function App() {
 
@@ -10,49 +9,40 @@ function App() {
   const [country, setCountry] = useState('USA');
   const [updateDatas, setUpdateDatas] = useState([]);
 
-  const {register, handleSubmit} = useForm();
-
-  useEffect(() => {
-    getData();
-  }, [month, year, country])
-
   const getData  = async () => {
-    const response = await fetch(`https://covidapi.info/api/v1/country/${country}/timeseries/${year}-${month}-1/${year}-${month}-31`);
+    const response = await fetch(`https://covidapi.info/api/v1/country/${country}/timeseries/${year}-${month}-1/${year}-${month}-30`);
 
     const data = await response.json();
     setUpdateDatas(data.result);
-    console.log(updateDatas);
   }
 
   const updateYear = e => {
     setYear(e.target.value);
-    console.log(year);
   };
 
   const updateMonth = e => {
     setMonth(e.target.value);
-    console.log(month);
   };
 
   const updateCountry = e => {
     setCountry(e.target.value);
-    console.log(country);
   };
 
-  const onSubmit = (dataForm) => {
-    console.log(dataForm.year);
-  }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getData(e);
+  }
 
   return (
     <div className="App">
       <div className="form-container">
-        <form className="chart-form" onSubmit={handleSubmit(onSubmit)} >
-          <input type="number" className="form-year" placeholder="select the year" name="year" min="2020" max="2021" value={year} onChange={updateYear} ref={register} />
-          <input type="number" className="form-month" placeholder="select the month" name="month" min="1" max="12" value={month} onChange={updateMonth} ref={register} />
+        <form className="chart-form" onSubmit={handleSubmit} >
+          <input type="number" className="form-year" placeholder="select the year" name="year" min="2020" max="2021" value={year} onChange={updateYear} />
+          <input type="number" className="form-month" placeholder="select the month" name="month" min="1" max="12" value={month} onChange={updateMonth} />
 
           <label htmlFor="country">Select the country</label>
-          <select name="country" value={country} onChange={updateCountry} ref={register} >
+          <select name="country" value={country} onChange={updateCountry} >
             <option value="IND">India</option>
             <option value="USA">United States of America</option>
             <option value="CZE">Czech Republic </option>
@@ -62,6 +52,7 @@ function App() {
       </div>
       <Chart updateDatas={updateDatas} />
     </div>
+
   );
 }
 
--- src/Chart.js
+++ src/Chart.js
@@ -3,7 +3,6 @@ import { Line } from 'react-chartjs-2';
 
 const Chart = ( {updateDatas} ) => {
 
-    // console.log(updateDatas);
     return (
         <div className="chart-line">
             <Line
